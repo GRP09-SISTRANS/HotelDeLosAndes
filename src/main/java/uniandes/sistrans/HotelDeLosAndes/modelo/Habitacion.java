04a17309081de5520d76dee69f45e634ac6dc766
Sebastian Sanchez <js.sanchezs12@uniandes.edu.co>
2023-10-01 00:40:15 -0500

    xD

---
+++ src/main/java/uniandes/sistrans/HotelDeLosAndes/modelo/Habitacion.java
@@ -0,0 +1,41 @@
+package uniandes.sistrans.HotelDeLosAndes.modelo;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Habitacion")
+public class Habitacion {
+
+
+    @Id
+    private Integer id;
+    private Integer capacidad;
+
+    public Habitacion(Integer Id, Integer Capacidad) {
+
+        this.id = Id;
+        this.capacidad = Capacidad;
+    }
+
+    public Habitacion()
+    {;}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+}
---
+++ src/main/java/uniandes/sistrans/HotelDeLosAndes/repositorios/UsuarioRepository.java
@@ -0,0 +1,24 @@
+package uniandes.sistrans.HotelDeLosAndes.repositorios;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import uniandes.sistrans.HotelDeLosAndes.modelo.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+    @Query(value = "SELECT * FROM Usuario WHERE id = :id", nativeQuery = true)
+    Usuario darUsuario(Integer id);
+
+    @Query(value = "SELECT * FROM Usuario", nativeQuery = true)
+    Collection<Usuario> darUsuarios();
+
+    @Query(value = "SELECT * FROM Usuario WHERE nombre = :nombre", nativeQuery = true)
+    Collection<Usuario> darUsuariosPorNombre(String nombre);
+    
+
+    
+}
