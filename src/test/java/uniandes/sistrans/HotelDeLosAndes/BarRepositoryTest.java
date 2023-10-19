5ce9645953443eaf4a3c3c793a07c7fc0fd3516f
javierb4rrera <js.barrerat1@uniandes.edu.co>
2023-10-18 23:06:03 -0500

    feat: :rocket: add Spring Security dependency for user authentication and authorization

--- src/test/java/uniandes/sistrans/HotelDeLosAndes/BarRepositoryTest.java
+++ src/test/java/uniandes/sistrans/HotelDeLosAndes/BarRepositoryTest.java
@@ -1,17 +1,29 @@
 package uniandes.sistrans.HotelDeLosAndes;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import uniandes.sistrans.HotelDeLosAndes.model.BarEntity;
+import uniandes.sistrans.HotelDeLosAndes.repositories.BarRepository;
 import uniandes.sistrans.HotelDeLosAndes.repositories.CuentaRepository;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-public class CuentaRepositoryTests {
+public class BarRepositoryTest {
+
     @Autowired
-    private CuentaRepository cuentaRepository;
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private BarRepository barRepository;
+
 }
