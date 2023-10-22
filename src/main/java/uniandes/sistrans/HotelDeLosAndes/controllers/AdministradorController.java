deedcefcc64d097f128bcb3f68e3d667df47165f
javierb4rrera <js.barrerat1@uniandes.edu.co>
2023-10-22 15:35:58 -0500

    refactor: :white_check_mark: delete unused imports and add RoleRepository

--- src/main/java/uniandes/sistrans/HotelDeLosAndes/controllers/AdministradorController.java
+++ src/main/java/uniandes/sistrans/HotelDeLosAndes/controllers/AdministradorController.java
@@ -1,9 +1,7 @@
 package uniandes.sistrans.HotelDeLosAndes.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
--- src/test/java/uniandes/sistrans/HotelDeLosAndes/BarRepositoryTest.java
+++ src/test/java/uniandes/sistrans/HotelDeLosAndes/BarRepositoryTest.java
@@ -1,29 +1,10 @@
 package uniandes.sistrans.HotelDeLosAndes;
 
-import java.util.List;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import uniandes.sistrans.HotelDeLosAndes.models.BarEntity;
-import uniandes.sistrans.HotelDeLosAndes.repositories.BarRepository;
-import uniandes.sistrans.HotelDeLosAndes.repositories.CuentaRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class BarRepositoryTest {
-
-    @Autowired
-    private TestEntityManager entityManager;
-
-    @Autowired
-    private BarRepository barRepository;
-
-}
+public class BarRepositoryTest {}
