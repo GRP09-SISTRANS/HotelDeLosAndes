820332ef7c89e9c8499e2d08e6e3bcff29bcb4a4
Feder1414 <f.bogota@uniandes.edu.co>
2023-11-02 18:53:39 -0500

    generar mas cosas

--- src/main/resources/db/dataGenerator.py
+++ src/main/resources/db/dataGenerator.py
@@ -1,6 +1,7 @@
 
 from faker import Faker
 import oracledb
+import random
 
 
 class DataGenerator:
@@ -9,7 +10,7 @@ class DataGenerator:
         self.cur = None
         self.conn = None
 
-        self.faker = Faker('es_ES')
+        self.faker = Faker('en_US')
         # define sequences
         self.secuencia_usuarios = 1
         self.secuencia_servicios = 1
@@ -19,6 +20,7 @@ class DataGenerator:
         self.secuencia_habitaciones = 1
         self.secuencia_cuenta = 1
         self.secuencia_producto = 1
+        self.secuencia_plan_consumo = 1000
 
     def create_connection(self, host: str, port: str, user: str, password: str):
         dsn_tns = oracledb.makedsn(host, port, service_name='PROD')
@@ -35,7 +37,7 @@ class DataGenerator:
             # tuple with the data to insert
             servicio_general = {
                 'id': self.secuencia_servicios,
-                'nombre': self.faker.name(),
+                'nombre': self.faker.word() + ' Tienda',
                 'tipo': 'Tienda'
             }
             
@@ -60,7 +62,7 @@ class DataGenerator:
         for i in range(cantidad_registros):
             servicio_general = {
                     'id': self.secuencia_servicios,
-                    'nombre': Faker('es_ES').name(),
+                    'nombre': self.faker.word(),
                     'tipo': 'Salon'
                 }
             servicio_salon = {'servicio_id': self.secuencia_servicios, 'capacidad': self.faker.random_int(min=1, max=100)}
@@ -82,11 +84,137 @@ class DataGenerator:
         self.cur.execute("DELETE FROM prestamo_utensilios")
         self.cur.execute("DELETE FROM spa")
         self.cur.execute("DELETE FROM supermercado")
+        self.cur.execute("DELETE FROM reserva_servicio")
+        self.cur.execute("DELETE FROM cuenta")
         self.cur.execute("DELETE FROM producto")
         self.cur.execute("DELETE FROM servicio")
+        
+        self.cur.execute("DELETE FROM usuario")
 
         self.conn.commit()
+    
+    def crearBar(self, cantidad_registros: int):
 
+        for i in range(cantidad_registros):
+            servicio_general = {
+                    'id': self.secuencia_servicios,
+                    'nombre': self.faker.word() + ' Bar',
+                    'tipo': 'Bar'
+                }
+            servicio_bar = {'servicio_id': self.secuencia_servicios, 'capacidad': self.faker.random_int(min=1, max=100), 'estilo': self.faker.word()}
+
+            self.cur.execute("INSERT INTO servicio (id, nombre, tipo) VALUES (:id, :nombre, :tipo)", servicio_general)
+            self.cur.execute("INSERT INTO bar (servicio_id, capacidad, estilo) VALUES (:servicio_id, :capacidad, :estilo)", servicio_bar)
+
+            self.secuencia_servicios += 1
+            self.conn.commit()
+
+    def crearPiscina(self, cantidad_registros: int):
+        for i in range(cantidad_registros):
+            servicio_general = {
+                    'id': self.secuencia_servicios,
+                    'nombre': self.faker.word() + ' Piscina',
+                    'tipo': 'Piscina'
+                }
+            servicio_piscina = {'servicio_id': self.secuencia_servicios, 'capacidad': self.faker.random_int(min=1, max=100), 'profundidad': self.faker.random_int(min=1, max=100)}
+
+            self.cur.execute("INSERT INTO servicio (id, nombre, tipo) VALUES (:id, :nombre, :tipo)", servicio_general)
+            self.cur.execute("INSERT INTO piscina (servicio_id, capacidad, profundidad) VALUES (:servicio_id, :capacidad, :profundidad)", servicio_piscina)
+            self.secuencia_servicios += 1
+            self.conn.commit()
+    
+    def crearGimnasio(self, cantidad_registros: int):
+        for i in range(cantidad_registros):
+            servicio_general = {
+                    'id': self.secuencia_servicios,
+                    'nombre': self.faker.word() + ' Gym',
+                    'tipo': 'Gimnasio'
+                }
+            servicio_gimnasio = {'servicio_id': self.secuencia_servicios, 'maquinas': self.faker.random_int(min=1, max=100)}
+
+            self.cur.execute("INSERT INTO servicio (id, nombre, tipo) VALUES (:id, :nombre, :tipo)", servicio_general)
+            self.cur.execute("INSERT INTO gimnasio (servicio_id, maquinas) VALUES (:servicio_id, :maquinas)", servicio_gimnasio)
+            self.secuencia_servicios += 1
+            self.conn.commit()
+    
+    def crearInternet(self, cantidad_registros: int):
+        for i in range(cantidad_registros):
+            servicio_general = {
+                    'id': self.secuencia_servicios,
+                    'nombre': self.faker.word() + ' Internet',
+                    'tipo': 'Internet'
+                }
+            servicio_internet = {'servicio_id': self.secuencia_servicios}
+
+            self.cur.execute("INSERT INTO servicio (id, nombre, tipo) VALUES (:id, :nombre, :tipo)", servicio_general)
+            self.cur.execute("INSERT INTO internet (servicio_id) VALUES (:servicio_id)", servicio_internet)
+            self.secuencia_servicios += 1
+            self.conn.commit()
+    
+    def crearProducto(self, cantidad_registro:int):
+        for i in range(cantidad_registro):
+            producto = {
+                'id': self.secuencia_producto,
+                'servicio_id': self.faker.random_int(min=1, max=self.secuencia_servicios-1),
+                'nombre': self.faker.word() + ' Producto',
+                'costo': self.faker.random_int(min=1, max=100000)
+            }
+            self.cur.execute("INSERT INTO producto (id, servicio_id, nombre, costo) VALUES (:id, :servicio_id, :nombre, :costo)", producto)
+            self.secuencia_producto += 1
+            self.conn.commit()
+    
+    def crearUsuario(self, cantidadRegistro:int, tipo_usuario:str):
+        tipos_documento = ['CC', 'CE', 'TI', 'PP']
+        for i in range(cantidadRegistro):
+            usuario = {
+                'id': self.secuencia_usuarios,
+                'tipo_doc': tipos_documento[random.randint(0,len(tipos_documento)-1)],
+                'correo': self.faker.email(),
+                'direccion': self.faker.address(),
+                'nombre': self.faker.name(),
+                'apellido': self.faker.last_name(),
+                'contrasenia': '123456789',
+                'tipo_usuario': tipo_usuario,
+                'plan_consumo_id': self.faker.random_int(min=1, max=self.secuencia_plan_consumo-1)
+            }
+            self.cur.execute("INSERT INTO usuario (id, tipo_doc, correo, direccion, nombre, apellido, contrasenia, tipo_usuario, plan_consumo_id) VALUES (:id, :tipo_doc, :correo, :direccion, :nombre, :apellido, :contrasenia, :tipo_usuario, :plan_consumo_id)", usuario)
+            self.secuencia_usuarios += 1
+            self.conn.commit()
+    
+    def crearReservaServicio(self, cantidadRegistro:int):
+        for i in range(cantidadRegistro):
+           
+            # Generar una fecha aleatoria
+            fecha = self.faker.date_between(start_date='-2y', end_date='today')
+            # Formatear la fecha para su inserción en Oracle
+            fecha_oracle = fecha.strftime('%Y-%m-%d').upper()
+            reserva_servicio = {
+                'id': self.secuencia_reservas_servicios,
+                'fecha': fecha_oracle, # 'DD-MON-YYYY
+                'producto_id': self.faker.random_int(min=1, max=self.secuencia_producto-1),
+                'usuario_id': self.faker.random_int(min=1, max=self.secuencia_usuarios-1)
+            }
+            self.cur.execute("INSERT INTO reserva_servicio (id, fecha, producto_id, usuario_id) VALUES (:id, to_date(:fecha, 'yyyy/mm/dd'), :producto_id, :usuario_id)", reserva_servicio)
+            self.secuencia_reservas_servicios += 1
+            self.conn.commit()
+    
+    def crearCuenta(self, cantidadRegistro:int):
+        fecha = self.faker.date_between(start_date='-2y', end_date='today')
+        for i in range(cantidadRegistro):
+            cuenta = {
+                'id': self.secuencia_cuenta,
+                'reserva_id': self.faker.random_int(min=1, max=self.secuencia_usuarios-1),
+                'producto_id': self.faker.random_int(min=1, max= self.secuencia_producto-1),
+                'cantidad': self.faker.random_int(min=1, max=100),
+                'fecha': fecha.strftime('%d-%b-%Y').upper(), # 'DD-MON-YYYY        
+            }
+            self.cur.execute("INSERT INTO cuenta (id, reserva_id, producto_id, cantidad, fecha) VALUES (:id, :reserva_id, :producto_id, :cantidad, :fecha)", cuenta)
+            self.secuencia_cuenta += 1
+            self.conn.commit()
+    
+
+    
+    
 
 if __name__ == '__main__':
     # instance the DataGenerator class
@@ -99,5 +227,13 @@ if __name__ == '__main__':
         user = 'ISIS2304D06202320',
         password= 'CfHmMdpRQPKQ'
     )
-    DataGenerator.limpiarBase(data_generator)
-    DataGenerator.crearSalonConferencia(data_generator, 100)
+    data_generator.limpiarBase()
+    #data_generator.crearSalonConferencia(10)
+    #data_generator.crearBar(10)
+    #data_generator.crearPiscina(10)
+    #data_generator.crearGimnasio(10)
+    data_generator.crearInternet(10)
+    data_generator.crearProducto(10)
+    data_generator.crearUsuario(10, 1)
+    data_generator.crearReservaServicio(10)
+
